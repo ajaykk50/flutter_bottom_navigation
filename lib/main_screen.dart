12fd3a9935d243491c8eb0eb292e8cd51e25f1ab
appke <appke@appkes-MacBook-Pro.local>
2022-05-18 14:45:28 +0530

    drop down done

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -3,12 +3,25 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class MainScreen extends StatelessWidget {
-  const MainScreen({Key? key}) : super(key: key);
+  MainScreen({Key? key}) : super(key: key);
+
+  final list = [
+    'Apple',
+    'Orange',
+    'Pine Apple',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Center(child: Text("Main Screen"))),
+      body: SafeArea(
+          child: Center(
+              child: DropdownButtonFormField(
+                  items: list.map((e) {
+                    return DropdownMenuItem(value: e, child: Text(e));
+                  }).toList(),
+                  hint: Text('Select Item'),
+                  onChanged: (value) {}))),
     );
   }
 }
